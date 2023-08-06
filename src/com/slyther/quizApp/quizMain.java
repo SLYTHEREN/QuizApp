ce191bb07ee4a36040d8dd337a180c478afadbe9
ShrijanPoudel <85357750+ShrijanPoudel@users.noreply.github.com>
2023-08-07 01:24:54 +0545

    Update quizMain.java

--- src/com/slyther/quizApp/quizMain.java
+++ src/com/slyther/quizApp/quizMain.java
@@ -4,7 +4,8 @@ public class quizMain {
 
 	/*
 	 * plan for the project: We need a quiz app to have questions multiple choices
-	 * probably audio and visuals and a next plane window
+	 * probably audio and visuals and a next plane window we need to work with
+	 * Jswing and other frameworks which i need to research on
 	 */
 
 }
