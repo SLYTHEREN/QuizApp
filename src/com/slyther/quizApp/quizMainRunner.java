deb159ea0927b40d848c3d226465cb1c04737854
ShrijanPoudel <85357750+ShrijanPoudel@users.noreply.github.com>
2023-08-07 01:26:06 +0545

    Small change

--- src/com/slyther/quizApp/quizMainRunner.java
+++ src/com/slyther/quizApp/quizMainRunner.java
@@ -1,16 +1,18 @@
 package com.slyther.quizApp;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-
 public class quizMainRunner {
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("My First GUI");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(300, 300);
-		JButton button = new JButton("Press");
-		frame.getContentPane().add(button); // Adds Button to content pane of frame
-		frame.setVisible(true);
+		/*
+		 * JFrame frame = new JFrame("My First GUI");
+		 * frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); frame.setSize(300,
+		 * 300); JButton button = new JButton("Press");
+		 * frame.getContentPane().add(button); // Adds Button to content pane of frame
+		 * frame.setVisible(true);
+		 * 
+		 * 
+		 * this is just a sample of a program in JFRam and java swing ( Need to work
+		 * more on that )
+		 */
 
 	}
 }
