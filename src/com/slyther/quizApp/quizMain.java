6b7f0320dd53cc99e5556567b4f27dd4fbc87984
ShrijanPoudel <85357750+ShrijanPoudel@users.noreply.github.com>
2023-08-07 01:22:52 +0545

    Initial commit

---
+++ src/com/slyther/quizApp/quizMain.java
@@ -0,0 +1,12 @@
+package com.slyther.quizApp;
+
+public class quizMain {
+
+	/*
+	 * plan for the project: We need a quiz app to have questions multiple choices
+	 * probably audio and visuals and a next plane window
+	 */
+
+}
+
+
