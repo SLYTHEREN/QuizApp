092c7dea9b93303ac6fbcd97646004dae04bc566
SLYTHEREN <141382313+SLYTHEREN@users.noreply.github.com>
2023-08-07 12:38:00 +0545

    Update quizMain.java

--- src/com/slyther/quizApp/quizMain.java
+++ src/com/slyther/quizApp/quizMain.java
@@ -6,6 +6,7 @@ public class quizMain {
 	 * plan for the project: We need a quiz app to have questions multiple choices
 	 * probably audio and visuals and a next plane window we need to work with
 	 * Jswing and other frameworks which i need to research on
+  better framework needed
 	 */
 
 }
