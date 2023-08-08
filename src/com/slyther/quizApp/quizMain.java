7c4e4d5a920597b30161f13654d717b3069932d2
SLYTHEREN <141382313+SLYTHEREN@users.noreply.github.com>
2023-08-08 08:35:40 +0545

    Update quizMain.java

--- src/com/slyther/quizApp/quizMain.java
+++ src/com/slyther/quizApp/quizMain.java
@@ -8,6 +8,17 @@ public class quizMain {
 	 * Jswing and other frameworks which i need to research on
   better framework needed
 	 */
+	private int request;
+	private boolean data; 
+	private String question;
+	private String mcq1;
+	private String mcq2;
+	private String mcq3;
+	private String mcq4;// just randomly setting the private string variables for multiple choice questions
+
+	// we need a detailed structure as well 
+
+	/* we need to set getters and setters */
 
 }
 
