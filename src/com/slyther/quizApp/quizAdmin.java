69fe4c0fd6e98db1635184edc108f4b1f265f745
SLYTHEREN <141382313+SLYTHEREN@users.noreply.github.com>
2023-08-09 13:52:25 +0545

    Update quizAdmin.java

--- src/com/slyther/quizApp/quizAdmin.java
+++ src/com/slyther/quizApp/quizAdmin.java
@@ -1,1 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class Admin {
+    private List<Question> questions;
+
+    public Admin() {
+        questions = new ArrayList<>();
+    }
+
+    public void addQuestion(Question question) {
+        questions.add(question);
+    }
+
+    // Other admin functionalities like saving to file, etc.
+}
