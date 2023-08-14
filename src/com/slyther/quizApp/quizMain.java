3a7a25109dc359ec3a42c676d738bcaa6e615629
SLYTHEREN <141382313+SLYTHEREN@users.noreply.github.com>
2023-08-14 08:32:22 +0545

    Update quizMain.java

--- src/com/slyther/quizApp/quizMain.java
+++ src/com/slyther/quizApp/quizMain.java
@@ -2,35 +2,7 @@ package com.slyther.quizApp;
 
 public class quizMain {
 
-	/*
-	 * plan for the project: We need a quiz app to have questions multiple choices
-	 * probably audio and visuals and a next plane window we need to work with
-	 * Jswing and other frameworks which i need to research on
-  better framework needed
-	 */
-	 private String questionText;
-    	private List<String> options;
-    	private int correctOptionIndex;
-    	private String multimediaPath;
-
-
-
 	
-	private int request;
-	private boolean data; 
-	private String question;
-	private String mcq1;
-	private String mcq2;
-	private String mcq3;
-	private String mcq4;// just randomly setting the private string variables for multiple choice questions
-
-	// we need a detailed structure as well 
-
-	/* we need to set getters and setters */
-	/* we need a structure about how the program is going to work: \
- 	qadd questions : 
-	 add options */
-
 }
 
 
